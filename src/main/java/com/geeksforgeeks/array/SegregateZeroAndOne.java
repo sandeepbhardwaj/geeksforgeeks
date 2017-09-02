ecf5c2cc2beb95cfe47d1581dfc3b7e10c57b211
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2017-09-02 23:26:16 +0530

    Segregate zero and one in given array of 0's and 1's

---
+++ src/main/java/com/geeksforgeeks/array/SegregateZeroAndOne.java
@@ -0,0 +1,37 @@
+package com.geeksforgeeks.array;
+
+public class SegregateZeroAndOne {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1 };
+
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start < end) {
+
+			// swap
+			if (arr[start] == 1 && arr[end] == 0) {
+				int temp = arr[start];
+				arr[start] = arr[end];
+				arr[end] = temp;
+
+				// increment start and decrement end
+				start++;
+				end--;
+			}
+
+			if (arr[start] == 1 && arr[end] != 0)
+				end--;
+
+			if (arr[start] != 1 && arr[end] == 0)
+				start++;
+
+		}
+
+		for (int element : arr) {
+			System.out.print(" " + element);
+		}
+	}
+
+}

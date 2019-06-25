3636378c814d439186d148dca04239fbe1b1e2f9
sbhardw8 <sandeep_bhardwaj@optum.com>
2019-06-25 16:43:43 +0530

    More programs

---
+++ src/main/java/leetcode/PalindromeNumber.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+/**
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 121
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ * <p>
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * Follow up:
+ * <p>
+ * Coud you solve it without converting the integer to a string?
+ */
+public class PalindromeNumber {
+	public boolean isPalindrome(int x) {
+		//for negative number no need to test
+		if (x < 0) {
+			return false;
+		}
+
+		int temp = x;
+
+		int reverseNumber = 0;
+		while (Math.abs(temp) > 0) {
+			reverseNumber = reverseNumber * 10 + temp % 10;
+			temp = temp / 10;
+		}
+
+		if (reverseNumber == x) {
+			return true;
+		}
+		return false;
+	}
+}
--- src/main/java/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,25 @@
 package leetcode;
 
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ * <p>
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * Example 1:
+ * <p>
+ * Given nums = [1,1,2],
+ * <p>
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * <p>
+ * It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ * <p>
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ * <p>
+ * Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+ * <p>
+ * It doesn't matter what values are set beyond the returned length.
+ */
 public class RemoveDuplicatesFromSortedArray {
 	public int removeDuplicates(int[] nums) {
 		int unique = 1;

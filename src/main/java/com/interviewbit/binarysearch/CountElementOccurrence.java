5dde70d3858531445c3163a2e28839554ff48dd7
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-06-29 23:12:55 +0530

    Count Element Occurrence

---
+++ src/main/java/com/interviewbit/binarysearch/CountElementOccurrence.java
@@ -0,0 +1,57 @@
+package com.interviewbit.binarysearch;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CountElementOccurrence {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+				1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+				2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+				3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+				4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+				5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+				6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+				7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+				8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+				9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10,
+				10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+				10, 10, 10, 10, 10, 10, 10);
+
+		int count = new CountElementOccurrence().findCount(list, 2);
+		System.out.println(count);
+	}
+
+	public int findCount(final List<Integer> A, int B) {
+		int start = searchIndex(A, B, 0, A.size() - 1, true);
+		int end = searchIndex(A, B, 0, A.size() - 1, false);
+
+		if (start == -1)
+			return 0;
+
+		int count = end - start + 1;
+		return count;
+	}
+
+	private int searchIndex(List<Integer> list, int B, int low, int high, boolean startIndex) {
+		int result = -1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+
+			if (list.get(mid) == B) {
+				result = mid;
+				if (startIndex)
+					high = mid - 1;
+				else
+					low = mid + 1;
+			} else if (list.get(mid) > B) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return result;
+	}
+
+}

f3dfd582696d5bb6ad4bfc7a924090dbae9ce510
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-07-06 23:39:42 +0530

    11. Container With Most Water

--- src/main/java/com/leetcode/ContainerWithMostWater.java
+++ src/main/java/com/leetcode/ContainerWithMostWater.java
@@ -13,6 +13,14 @@ package com.leetcode;
  * <p>
  * Input: [1,8,6,2,5,4,8,3,7]
  * Output: 49
+ * <p>
+ * Idea / Proof:
+ * <p>
+ * The widest container (using first and last line) is a good candidate, because of its width. Its water level is the
+ * height of the smaller one of first and last line.
+ * All other containers are less wide and thus would need a higher water level in order to hold more water.
+ * The smaller one of first and last line doesn't support a higher water level and can thus be safely removed from further
+ * consideration.
  */
 public class ContainerWithMostWater {
 	public int maxArea(int[] height) {

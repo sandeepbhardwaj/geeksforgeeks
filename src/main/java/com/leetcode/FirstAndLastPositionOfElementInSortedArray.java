f814addd95fee6d7b12b76f788126686a1143f5b
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-07-12 11:50:37 +0530

    Added program number

--- src/main/java/com/leetcode/FirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/leetcode/FirstAndLastPositionOfElementInSortedArray.java
@@ -1,6 +1,8 @@
 package com.leetcode;
 
 /**
+ * 34. Find First and Last Position of Element in Sorted Array
+ * <p>
  * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
  * <p>
  * Your algorithm's runtime complexity must be in the order of O(log n).
@@ -47,5 +49,4 @@ class FirstAndLastPositionOfElementInSortedArray {
 		}
 		return index;
 	}
-
 }

177d9df9f9b0d3ad941db028c0eee5b68af650cb
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-08-27 10:51:35 +0530

    Added comments

--- src/main/java/com/leetcode/SearchInRotatedSortedArrayWithDuplicates.java
+++ src/main/java/com/leetcode/SearchInRotatedSortedArrayWithDuplicates.java
@@ -17,6 +17,13 @@ package com.leetcode;
  * <p>
  * Input: nums = [2,5,6,0,0,1,2], target = 3
  * Output: false
+ * <p>
+ * The only difference is that due to the existence of duplicates, we can have nums[left] == nums[mid]
+ * and in that case, the first half could be out of order (i.e. NOT in the ascending order,
+ * e.g. [3 1 2 3 3 3 3]) and we have to deal this case separately.
+ * In that case, it is guaranteed that nums[right] also equals to nums[mid], so what we can do is to check
+ * if nums[mid]== nums[left] == nums[right] before the original logic,
+ * and if so, we can move left and right both towards the middle by 1. and repeat.
  */
 public class SearchInRotatedSortedArrayWithDuplicates {
 

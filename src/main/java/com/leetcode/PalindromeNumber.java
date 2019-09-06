1b27dee150a4016e15089529f2f06d60f1c79717
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-09-06 09:55:07 +0530

    typo fix

--- src/main/java/com/leetcode/PalindromeNumber.java
+++ src/main/java/com/leetcode/PalindromeNumber.java
@@ -19,7 +19,7 @@ package com.leetcode;
  * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
  * Follow up:
  * <p>
- * Coud you solve it without converting the integer to a string?
+ * Could you solve it without converting the integer to a string?
  */
 public class PalindromeNumber {
 	public boolean isPalindrome(int x) {

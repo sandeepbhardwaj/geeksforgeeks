0bf396b71b784db38a3bb187199838acaa16d0b4
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-09-21 21:46:01 +0530

    21. Merge Two Sorted Lists

--- src/main/java/com/leetcode/TwoSum.java
+++ src/main/java/com/leetcode/TwoSum.java
@@ -2,6 +2,19 @@ package com.leetcode;
 
 import java.util.*;
 
+/**
+ * 1. Two Sum
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * <p>
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
 public class TwoSum {
 	public int[] twoSum(int[] nums, int target) {
 		// contains value and its index
@@ -17,10 +30,11 @@ public class TwoSum {
 
 			// second condition is most important to discard the same element
 			if (map.containsKey(find) && map.get(find) != i) {
-				return new int[] { i, map.get(find) };
+				return new int[]{i, map.get(find)};
 			}
 
 		}
 		return null;
 	}
+
 }

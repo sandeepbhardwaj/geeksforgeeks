91c8d4260f6fffc71d285ba6eea12d5228d72a94
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-10-13 18:04:01 +0530

    560. Subarray Sum Equals K

---
+++ src/main/java/com/leetcode/array/SubarraySumEqualsK.java
@@ -0,0 +1,49 @@
+package com.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 560. Subarray Sum Equals K
+ * 
+ * Given an array of integers and an integer k, you need to find the total
+ * number of continuous subarrays whose sum equals to k.
+ * 
+ * Example 1: Input:nums = [1,1,1], k = 2 Output: 2
+ * 
+ * @author sandeep
+ *
+ */
+public class SubarraySumEqualsK {
+
+	public static void main(String[] args) {
+		int[] nums = { 1 };
+		System.out.println(subarraySum(nums, 0));
+
+	}
+
+	public static int subarraySum(int[] nums, int k) {
+
+		// map contains sum and its frequency
+		Map<Integer, Integer> preSum = new HashMap<>();
+
+		// this default value needed when we found sum-k=0
+		preSum.put(0, 1);
+
+		int sum = 0;
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+
+			// if sum-k present in map increment the count
+			count += preSum.getOrDefault(sum - k, 0);
+
+			// add the sum in map and increment the frequency by 1
+			preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+
+		}
+		return count;
+
+	}
+
+}

5fd8c0b6437aaf3de6d38d4b6d606b9f8c411d3b
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2019-11-14 17:54:52 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- src/main/java/com/leetcode/array/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/leetcode/array/RemoveDuplicatesFromSortedArray.java
@@ -29,7 +29,7 @@ public class RemoveDuplicatesFromSortedArray {
 	public int removeDuplicates(int[] nums) {
 		int unique = 1;
 		for (int i = 1; i < nums.length; i++) {
-			if (nums[i - 1] != nums[i]) {
+			if (nums[i] != nums[i - 1]) {
 				nums[unique] = nums[i];
 				unique++;
 			}

070eaf06a286f88f904445b38582467feb1c7dd9
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-11-21 19:58:26 +0530

    New programs

---
+++ src/main/java/com/interview/ReverseStringRecurssion.java
@@ -0,0 +1,18 @@
+package com.interview;
+
+public class ReverseStringRecurssion {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+
+		str = reverse(str);
+		System.out.println(str);
+	}
+
+	public static String reverse(String str) {
+		if ((str == null) || (str.length() == 0))
+			return str;
+		return reverse(str.substring(1)) + str.charAt(0);
+	}
+
+}

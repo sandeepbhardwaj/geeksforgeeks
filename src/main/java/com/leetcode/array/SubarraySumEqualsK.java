4ca020e6bf46602641f33e382f40efc1959ceb75
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2021-04-22 18:35:00 +0530

    Update SubarraySumEqualsK.java

--- src/main/java/com/leetcode/array/SubarraySumEqualsK.java
+++ src/main/java/com/leetcode/array/SubarraySumEqualsK.java
@@ -16,10 +16,18 @@ import java.util.Map;
 public class SubarraySumEqualsK {
 
 	public static void main(String[] args) {
-		int[] nums = {1};
-		System.out.println(subarraySum(nums, 0));
+		int[] nums = {1, 0, 1, 0, 1};
+		System.out.println(subarraySum(nums, 1));
 	}
 
+	/**
+	 * if the cumulative sum up to two indices, say ii and jj is at a difference of k i.e.
+	 * if sum[i] - sum[j] = k
+	 *
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
 	public static int subarraySum(int[] nums, int k) {
 		// map contains sum and its frequency
 		Map<Integer, Integer> preSum = new HashMap<>();
@@ -42,4 +50,27 @@ public class SubarraySumEqualsK {
 		return count;
 	}
 
+	/**
+	 * consider every possible subarray of the given numsnums array, find the sum of the elements of each of those
+	 * subarrays and check for the equality of the sum obtained with the given kk. Whenever the sum equals kk,
+	 * we can increment the count used to store the required result.
+	 * <p>
+	 * Time complexity : O(n^2). We need to consider every subarray possible.
+	 *
+	 * @param nums
+	 * @param k
+	 * @return count
+	 */
+	public static int bruteForce(int[] nums, int k) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			int sum = 0;
+			for (int j = i; j < nums.length; j++) {
+				sum += nums[j];
+				if (sum == k)
+					count++;
+			}
+		}
+		return count;
+	}
 }

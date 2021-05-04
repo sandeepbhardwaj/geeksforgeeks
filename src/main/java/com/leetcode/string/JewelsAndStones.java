51f1bda5d61aab3347cdbe2459dce24278b76736
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2021-05-04 14:06:07 +0530

    Update JewelsAndStones.java

--- src/main/java/com/leetcode/string/JewelsAndStones.java
+++ src/main/java/com/leetcode/string/JewelsAndStones.java
@@ -18,8 +18,14 @@ package com.leetcode.string;
  * Output: 0
  */
 public class JewelsAndStones {
+	public static void main(String[] args) {
+		String jewels = "aA", stones = "aAAbbbb";
 
-	public int numJewelsInStones(String jewels, String stones) {
+		int noOfJewels = numJewelsInStones(jewels, stones);
+		System.out.println(noOfJewels);
+	}
+
+	public static int numJewelsInStones(String jewels, String stones) {
 		//create array to hold all alphabets
 
 		boolean[] markedJewels = new boolean['z' + 1];

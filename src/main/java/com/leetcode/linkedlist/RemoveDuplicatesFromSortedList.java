e450b0177d207fcedfa0a038cbd6e7bc0e870d6a
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-05-22 23:09:28 +0530

    Update RemoveDuplicatesFromSortedList.java

--- src/main/java/com/leetcode/linkedlist/RemoveDuplicatesFromSortedList.java
+++ src/main/java/com/leetcode/linkedlist/RemoveDuplicatesFromSortedList.java
@@ -17,17 +17,13 @@ import com.leetcode.ListNode;
 public class RemoveDuplicatesFromSortedList {
 	public ListNode deleteDuplicates(ListNode head) {
 		ListNode current = head;
-		while (current != null) {
-			if (current.next == null)
-				break;
-
+		while (current != null && current.next != null) {
 			if (current.val == current.next.val) {
-				// removing duplicate breaking the link
+				//removing duplicate breaking the link
 				current.next = current.next.next;
 			} else {
 				current = current.next;
 			}
-
 		}
 		return head;
 	}

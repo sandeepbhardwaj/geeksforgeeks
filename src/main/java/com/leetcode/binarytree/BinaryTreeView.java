ff2e27b6bebdb9af19a4e323bbe29b2b32c8bbfc
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-05-24 23:19:06 +0530

    Left and Right view of Binary Tree

---
+++ src/main/java/com/leetcode/binarytree/BinaryTreeView.java
@@ -0,0 +1,62 @@
+package com.leetcode.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 199. Binary Tree Right Side View
+ */
+public class BinaryTreeView {
+
+	public List<Integer> leftSideView(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
+
+		if (root == null)
+			return result;
+
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+
+				//print first element from queue
+				if (i == 0) result.add(node.val);
+
+				if (node.left != null) queue.add(node.left);
+				if (node.right != null) queue.add(node.right);
+			}
+		}
+		return result;
+	}
+
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
+
+		if (root == null)
+			return result;
+
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+
+				//print last element from queue
+				if (i == size - 1) result.add(node.val);
+
+				if (node.left != null) queue.add(node.left);
+				if (node.right != null) queue.add(node.right);
+			}
+		}
+		return result;
+	}
+
+
+}

2d3bc254831bb7861375fcb31b7ccd1223023f6f
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-05-26 12:55:42 +0530

    Create VerticalOrderTraversal.java

---
+++ src/main/java/com/leetcode/binarytree/VerticalOrderTraversal.java
@@ -0,0 +1,57 @@
+package com.leetcode.binarytree;
+
+import java.util.*;
+
+public class VerticalOrderTraversal {
+
+	/**
+	 * Vertical order traversal using Level order + HasTable
+	 * @param root
+	 * @return
+	 */
+	public List<List<Integer>> verticalTraversal(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (root == null) {
+			result.add(new ArrayList<>());
+			return result;
+		}
+
+		//adding height and list of nodes as value
+		TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+
+		Queue<QNode> queue = new LinkedList<>();
+
+		queue.add(new QNode(root, 0));
+
+		while (!queue.isEmpty()) {
+			QNode qNode = queue.poll();
+
+			List<Integer> list = map.getOrDefault(qNode.hd, new ArrayList<>());
+			list.add(qNode.node.val);
+			map.put(qNode.hd, list);
+
+			if (qNode.node.left != null) queue.add(new QNode(qNode.node.left, qNode.hd - 1));
+			if (qNode.node.right != null) queue.add(new QNode(qNode.node.right, qNode.hd + 1));
+		}
+
+		for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+			List<Integer> val = entry.getValue();
+			result.add(val);
+		}
+
+		return result;
+	}
+
+	/**
+	 * Class to store TreeNode and horizontal distance of node form root
+	 */
+	class QNode {
+		TreeNode node;
+		int hd;
+
+		QNode(TreeNode node, int hd) {
+			this.node = node;
+			this.hd = hd;
+		}
+	}
+}

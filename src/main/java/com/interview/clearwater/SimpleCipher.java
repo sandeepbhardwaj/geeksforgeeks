c7cc16aae5e44ca81569824099bfb9b91a20b335
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-05-31 19:56:32 +0530

    Create SimpleCipher.java

---
+++ src/main/java/com/interview/clearwater/SimpleCipher.java
@@ -0,0 +1,61 @@
+package com.interview.clearwater;
+
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
+public class SimpleCipher {
+
+	// (A,1) = Z
+	public static String leftRotate(String str, int d) {
+		StringBuilder result = new StringBuilder();
+		d = d % 26;
+		for (Character ch : str.toCharArray()) {
+			if (!isAlpha(ch)) {
+				result.append(ch);
+				continue;
+			}
+			//left rotate
+			int index = ch - d;
+
+			if ((isUpperCase(ch) && index < 65) || (isLowerCase(ch) && index < 97)) {
+				index = index + 26;
+			}
+			result.append((char) index);
+		}
+		return result.toString();
+	}
+
+	//(Z,1) = A
+	public static String rightRotate(String str, int d) {
+		StringBuilder result = new StringBuilder();
+		d = d % 26;
+		for (Character ch : str.toCharArray()) {
+			if (!isAlpha(ch)) {
+				result.append(ch);
+				continue;
+			}
+			//rotate right
+			int index = ch + d;
+
+			//index moves to out of max range then reduce to 26 char
+			if ((isUpperCase(ch) && index > 90) || (isLowerCase(ch) && index > 122)) {
+				index = index - 26;
+			}
+			result.append((char) index);
+		}
+		return result.toString();
+	}
+
+
+	// 65 to 90 => UpperCase A to Z
+	// 97 to 122 => LowerCase a to z
+	private static boolean isAlpha(Character ch) {
+		return ch >= 65 && ch <= 122;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("left Rotate (A,2):" + leftRotate("A", 2) + ", Right Rotate (Y,2):" + rightRotate("Y", 2));
+	}
+
+}

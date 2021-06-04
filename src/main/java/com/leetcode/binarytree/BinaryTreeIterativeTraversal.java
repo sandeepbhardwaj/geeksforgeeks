7b95fe75700d79c278f682f58d1f27919c35fc4b
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2021-06-04 13:22:56 +0530

    Update BinaryTreeIterativeTraversal.java

--- src/main/java/com/leetcode/binarytree/BinaryTreeIterativeTraversal.java
+++ src/main/java/com/leetcode/binarytree/BinaryTreeIterativeTraversal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class InOrderTreeTraversal {
+public class BinaryTreeIterativeTraversal {
 	public List<Integer> inorderTraversal(TreeNode root) {
 		List<Integer> result = new ArrayList<>();
 		Stack<TreeNode> stack = new Stack<>();
@@ -25,6 +25,28 @@ public class InOrderTreeTraversal {
 			current = current.right;
 		}
 
+		return result;
+	}
+
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		if (root == null)
+			return result;
+
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+			//pop root element
+			TreeNode node = stack.pop();
+			result.add(node.val);
+
+			if (node.right != null)
+				stack.push(node.right);
+
+			if (node.left != null)
+				stack.push(node.left);
+		}
 		return result;
 	}
 }

9b5be8e1d14b520b318ff347586bda73621b28f3
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-06-06 22:27:40 +0530

    Update IntegerToRoman.java

--- src/main/java/com/leetcode/array/IntegerToRoman.java
+++ src/main/java/com/leetcode/array/IntegerToRoman.java
@@ -95,18 +95,4 @@ public class IntegerToRoman {
 			this.value = value;
 		}
 	}
-
-	/**
-	 * Method 2
-	 *
-	 * @param num
-	 * @return
-	 */
-	public static String intToRoman_2(int num) {
-		String[] thousands = {"", "M", "MM", "MMM"};
-		String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-		String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-		String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-		return thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] + units[num % 10];
-	}
 }

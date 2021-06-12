9b23eba5611684fd6a5adddc67584c685c682d8e
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-06-12 23:43:33 +0530

    Update LongestSubStrWithoutRepChars.java

--- src/main/java/com/leetcode/string/LongestSubStrWithoutRepChars.java
+++ src/main/java/com/leetcode/string/LongestSubStrWithoutRepChars.java
@@ -6,23 +6,20 @@ import java.util.Set;
 /**
  * 3. Longest Substring Without Repeating Characters
  * <p>
- * Given a string, find the length of the longest substring without repeating
- * characters.
+ * Given a string, find the length of the longest substring without repeating characters.
  * <p>
  * Example 1:
+ * Input: "abcabcbb" Output: 3 Explanation: The answer is "abc", with the length of 3.
  * <p>
- * Input: "abcabcbb" Output: 3 Explanation: The answer is "abc", with the length
- * of 3. Example 2:
+ * Example 2:
+ * Input: "bbbbb" Output: 1 Explanation: The answer is "b", with the length of 1.
  * <p>
- * Input: "bbbbb" Output: 1 Explanation: The answer is "b", with the length of
- * 1. Example 3:
- * <p>
- * Input: "pwwkew" Output: 3 Explanation: The answer is "wke", with the length
- * of 3. Note that the answer must be a substring, "pwke" is a subsequence and
- * not a substring.
+ * Example 3:
+ * Input: "pwwkew" Output: 3 Explanation: The answer is "wke", with the length of 3.
+ * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class LongestSubStrWithoutRepChars {
-	public int lengthOfLongestSubstring(String s) {
+	public static int lengthOfLongestSubstring(String s) {
 		// base case
 		if (s == null || s.length() == 0) {
 			return 0;
@@ -34,7 +31,7 @@ public class LongestSubStrWithoutRepChars {
 		int max = 0;
 
 		while (right < s.length() && left <= right) {
-			// if element not exist in set add and return true
+			//add and return true if set did not already contain the specified element
 			if (set.add(s.charAt(right))) {
 				// move to next element to increase window size
 				right++;
@@ -47,5 +44,9 @@ public class LongestSubStrWithoutRepChars {
 
 		}
 		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(lengthOfLongestSubstring("pwwkew"));
 	}
 }

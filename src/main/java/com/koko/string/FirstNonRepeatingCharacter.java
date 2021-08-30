0945e9aedd4e9e923d516ea0df69fcfabc711a12
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2021-08-30 12:45:16 +0530

    Update FirstNonRepeatingCharacter.java

--- src/main/java/com/koko/string/FirstNonRepeatingCharacter.java
+++ src/main/java/com/koko/string/FirstNonRepeatingCharacter.java
@@ -46,4 +46,37 @@ public class FirstNonRepeatingCharacter {
 		throw new RuntimeException("didn't find any non repeated Character");
 	}
 
+	/**
+	 * 387. First Unique Character in a String
+	 * Given a string s, find the first non-repeating character in it and return its index. If it does not exist,
+	 * return -1.
+	 * <p>
+	 * Example 1:
+	 * Input: s = "leetcode"
+	 * Output: 0
+	 * <p>
+	 * Example 2:
+	 * Input: s = "loveleetcode"
+	 * Output: 2
+	 * <p>
+	 * Example 3:
+	 * Input: s = "aabb"
+	 * Output: -1
+	 *
+	 * @param s
+	 * @return
+	 */
+	public int firstUniqChar(String s) {
+		int freq[] = new int[26];
+
+		for (int i = 0; i < s.length(); i++) {
+			freq[s.charAt(i) - 'a']++;
+		}
+
+		for (int i = 0; i < s.length(); i++) {
+			if (freq[s.charAt(i) - 'a'] == 1)
+				return i;
+		}
+		return -1;
+	}
 }

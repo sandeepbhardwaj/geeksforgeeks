1508e8e98e9e8c0244d199c4342e4a8f58d5ffc5
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-09-04 21:26:48 +0530

    Update KnapsackProblem.java

--- src/main/java/com/koko/dp/KnapsackProblem.java
+++ src/main/java/com/koko/dp/KnapsackProblem.java
@@ -61,6 +61,33 @@ public class KnapsackProblem {
 			return dp[n][w] = knapsackRecursionMemoization(wt, val, w, n - 1, dp);
 	}
 
+	/**
+	 * Top-down approach
+	 *
+	 * @param wt
+	 * @param val
+	 * @param w
+	 * @param n
+	 * @return
+	 */
+	public static int knapsack(int wt[], int val[], int w, int n) {
+		int i, j;
+		int dp[][] = new int[n + 1][w + 1];
+
+		for (i = 0; i <= n; i++) {
+			for (j = 0; j <= w; j++) {
+				if (i == 0 || j == 0) {
+					dp[i][j] = 0;
+				} else if (wt[i - 1] <= j) {
+					dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+				} else {
+					dp[i][j] = dp[i - 1][j];
+				}
+			}
+		}
+		return dp[n][w];
+	}
+
 	// Driver code
 	public static void main(String[] args) {
 		int val[] = new int[]{60, 100, 120};
@@ -78,5 +105,7 @@ public class KnapsackProblem {
 		Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
 
 		System.out.println("Max total value in knapsack :" + knapsackRecursionMemoization(wt, val, w, n, dp));
+
+		System.out.println("Max total value in knapsack using top-down approach :" + knapsack(wt, val, w, n));
 	}
 }

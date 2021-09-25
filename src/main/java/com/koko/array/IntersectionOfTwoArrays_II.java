db65d70e3e659f75b58d420591b16cf312581dca
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-09-25 08:02:53 +0530

    Reformat code

--- src/main/java/com/koko/array/IntersectionOfTwoArrays_II.java
+++ src/main/java/com/koko/array/IntersectionOfTwoArrays_II.java
@@ -27,24 +27,24 @@ import java.util.List;
  * link : https://leetcode.com/problems/intersection-of-two-arrays-ii/discuss/282372/Java-solution-with-all-3-follow-up-questions
  */
 public class IntersectionOfTwoArrays_II {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i : nums1) {
-            int freq = map.getOrDefault(i, 0);
-            map.put(i, freq + 1);
-        }
+	public int[] intersect(int[] nums1, int[] nums2) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i : nums1) {
+			int freq = map.getOrDefault(i, 0);
+			map.put(i, freq + 1);
+		}
 
-        List<Integer> list = new ArrayList<>();
-        for (int i : nums2) {
-            if (map.get(i) != null && map.get(i) > 0) {
-                list.add(i);
-                map.put(i, map.get(i) - 1);
-            }
-        }
-        int[] ret = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            ret[i] = list.get(i);
-        }
-        return ret;
-    }
+		List<Integer> list = new ArrayList<>();
+		for (int i : nums2) {
+			if (map.get(i) != null && map.get(i) > 0) {
+				list.add(i);
+				map.put(i, map.get(i) - 1);
+			}
+		}
+		int[] ret = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			ret[i] = list.get(i);
+		}
+		return ret;
+	}
 }
--- src/main/java/com/koko/array/MergeSortedArray.java
+++ src/main/java/com/koko/array/MergeSortedArray.java
@@ -11,15 +11,15 @@ package com.koko.array;
  * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
  */
 public class MergeSortedArray {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
 
-        while (tail1 >= 0 && tail2 >= 0) {
-            nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? nums1[tail1--] : nums2[tail2--];
-        }
+		while (tail1 >= 0 && tail2 >= 0) {
+			nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? nums1[tail1--] : nums2[tail2--];
+		}
 
-        while (tail2 >= 0) { //only need to combine with remaining nums2
-            nums1[finished--] = nums2[tail2--];
-        }
-    }
+		while (tail2 >= 0) { //only need to combine with remaining nums2
+			nums1[finished--] = nums2[tail2--];
+		}
+	}
 }

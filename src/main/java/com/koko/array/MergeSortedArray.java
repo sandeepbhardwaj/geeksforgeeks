46f63cf04fe76260495744cd5d3cbdb848f4727d
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2021-09-25 09:56:59 +0530

    Update MergeSortedArray.java

--- src/main/java/com/koko/array/MergeSortedArray.java
+++ src/main/java/com/koko/array/MergeSortedArray.java
@@ -22,4 +22,28 @@ public class MergeSortedArray {
 			nums1[finished--] = nums2[tail2--];
 		}
 	}
+
+	public void merge_1(int[] nums1, int m, int[] nums2, int n) {
+		int size = m + n - 1; //result size
+		m = m - 1;
+		n = n - 1;
+
+		while (m >= 0 && n >= 0) {
+			if (nums1[m] > nums2[n]) {
+				nums1[size] = nums1[m];
+				size--;
+				m--;
+			} else {
+				nums1[size] = nums2[n];
+				size--;
+				n--;
+			}
+		}
+
+		while (n >= 0) {
+			nums1[size] = nums2[n];
+			size--;
+			n--;
+		}
+	}
 }

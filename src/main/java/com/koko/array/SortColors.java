e1c6871a7202a3cba29b592fba90b1d29c1a7699
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-01-05 14:54:21 +0530

    Update SortColors.java

--- src/main/java/com/koko/array/SortColors.java
+++ src/main/java/com/koko/array/SortColors.java
@@ -1,5 +1,22 @@
 package com.koko.array;
 
+/**
+ * 75. Sort Colors
+ * Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color
+ * are adjacent, with the colors in the order red, white, and blue.
+ * <p>
+ * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+ * <p>
+ * You must solve this problem without using the library's sort function.
+ * <p>
+ * Example 1:
+ * Input: nums = [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * <p>
+ * Example 2:
+ * Input: nums = [2,0,1]
+ * Output: [0,1,2]
+ */
 public class SortColors {
 	public void sortColors(int[] nums) {
 		int low = 0;

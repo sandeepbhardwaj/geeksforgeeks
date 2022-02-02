e111762ce4064812084f1901c6ba542d8e89b72d
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2022-02-02 17:02:16 +0530

    Update MergeTwoBinaryTrees.java

--- src/main/java/com/koko/binarytree/MergeTwoBinaryTrees.java
+++ src/main/java/com/koko/binarytree/MergeTwoBinaryTrees.java
@@ -43,4 +43,18 @@ public class MergeTwoBinaryTrees {
 		return newNode;
 
 	}
+
+	public TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
+		//if check is important
+		if (root1 == null)
+			return root2;
+		if (root2 == null)
+			return root1;
+
+		root1.val = root1.val + root2.val;
+
+		root1.left = mergeTrees1(root1.left, root2.left);
+		root1.right = mergeTrees1(root1.right, root2.right);
+		return root1;
+	}
 }

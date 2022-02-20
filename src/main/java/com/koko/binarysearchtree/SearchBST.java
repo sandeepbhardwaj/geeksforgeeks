c05164e6bb34d0a3267af74f3eb3e0a16339af8b
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-02-20 12:36:14 +0530

    Update SearchBST.java

--- src/main/java/com/koko/binarysearchtree/SearchBST.java
+++ src/main/java/com/koko/binarysearchtree/SearchBST.java
@@ -2,6 +2,13 @@ package com.koko.binarysearchtree;
 
 import com.koko.TreeNode;
 
+/**
+ * 700. Search in a Binary Search Tree
+ * You are given the root of a binary search tree (BST) and an integer val.
+ * <p>
+ * Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+ * If such a node does not exist, return null.
+ */
 public class SearchBST {
 	public TreeNode searchBST(TreeNode root, int val) {
 

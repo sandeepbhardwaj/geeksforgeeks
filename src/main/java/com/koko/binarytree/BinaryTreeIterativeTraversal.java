3e7b510fbf75d5da5009391846b066119bea9034
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-03-12 21:30:11 +0530

    Post order traversal using two stacks

--- src/main/java/com/koko/binarytree/BinaryTreeIterativeTraversal.java
+++ src/main/java/com/koko/binarytree/BinaryTreeIterativeTraversal.java
@@ -27,42 +27,97 @@ public class BinaryTreeIterativeTraversal {
 		return result;
 	}
 
+
 	public List<Integer> inorderTraversal(TreeNode root) {
 		List<Integer> result = new ArrayList<>();
+
+		if (root == null)
+			return result;
+
 		Stack<TreeNode> stack = new Stack<>();
-
 		TreeNode current = root;
-		while (current != null || !stack.isEmpty()) {
 
-			//add and move to left till end of tree
+		while (!stack.isEmpty() || current != null) {
+			//loop to go extreme left
 			while (current != null) {
-				stack.push(current);
+				stack.add(current);
 				current = current.left;
 			}
 
-			//once all left ends pop from stack
-			current = stack.pop();
-			result.add(current.val);
-			//go to right tree
-			current = current.right;
+			//get the element from stack
+			TreeNode node = stack.pop();
+			result.add(node.val);
+
+			//get the right of node if its null the above loop will not execute
+			//and we will pop the element from stack
+			current = node.right;
 		}
 
 		return result;
 	}
 
-	public List<Integer> postorderTraversal(TreeNode root) {
-		Stack<TreeNode> stack = new Stack<>();
+
+	/**
+	 * Simple solution
+	 *
+	 * @param root
+	 * @return
+	 */
+	public List<Integer> postorderTraversalUsingTwoStack(TreeNode root) {
 		List<Integer> result = new ArrayList<>();
-		TreeNode current = root;
-		while (current != null || !stack.isEmpty()) {
-			if (current != null) {
-				stack.add(current);
-				result.add(0, current.val);
-				current = current.right;
-			} else {
-				current = stack.pop();
-				current = current.left;
-			}
+		if (root == null)
+			return result;
+
+		Stack<TreeNode> stack1 = new Stack<>();
+		Stack<TreeNode> stack2 = new Stack<>();
+
+		//add root to stack1
+		stack1.add(root);
+
+		while (!stack1.isEmpty()) {
+			TreeNode node = stack1.pop();
+
+			//add node to stack2
+			stack2.add(node);
+
+			//add left first and then right
+			if (node.left != null) stack1.add(node.left);
+			if (node.right != null) stack1.add(node.right);
+
+		}
+
+		//stack2 holds the result;
+		while (!stack2.isEmpty()) {
+			result.add(stack2.pop().val);
+		}
+
+		return result;
+	}
+
+	/**
+	 * Using single stack
+	 * @param root
+	 * @return
+	 */
+	public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		if (root == null)
+			return result;
+
+		Stack<TreeNode> stack = new Stack<>();
+
+		//add root to stack
+		stack.add(root);
+
+		while (!stack.isEmpty()) {
+			TreeNode node = stack.pop();
+
+			//add node value to front
+			result.add(0, node.val);
+
+			//add left first and then right
+			if (node.left != null) stack.add(node.left);
+			if (node.right != null) stack.add(node.right);
 		}
 		return result;
 	}

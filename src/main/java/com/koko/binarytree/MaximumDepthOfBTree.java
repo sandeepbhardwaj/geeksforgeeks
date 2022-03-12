8429f7595c2246832e70cf5136235ddebe8f273a
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-03-12 21:29:52 +0530

    Update MaximumDepthOfBTree.java
    "Top-down" Solution >> preOrder traversal | top_down(root, params)
    "Bottom-up" Solution >> postOrder traversal | bottom_up(root)

--- src/main/java/com/koko/binarytree/MaximumDepthOfBTree.java
+++ src/main/java/com/koko/binarytree/MaximumDepthOfBTree.java
@@ -16,10 +16,35 @@ import com.koko.TreeNode;
  * Given binary tree [3,9,20,null,null,15,7], return its depth = 3.
  */
 public class MaximumDepthOfBTree {
+
+	//"Bottom-up" Solution >> postOrder traversal | bottom_up(root)
 	public int maxDepth(TreeNode root) {
+
 		if (root == null)
 			return 0;
 
-		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+		int left_depth = maxDepth(root.left);
+		int right_depth = maxDepth(root.right);
+
+		// +1 because root is not null so depth should be greater than 1
+		return 1 + Math.max(left_depth, right_depth);
+
 	}
+
+	//"Top-down" Solution >> preOrder traversal | top_down(root, params)
+	int maxDepth = 0;
+
+	public void maxDepthTopDown(TreeNode root, int depth) {
+
+		if (root == null)
+			return;
+
+		if (root.left == null && root.right == null) {
+			maxDepth = Math.max(maxDepth, depth);
+		}
+		maxDepthTopDown(root.left, depth + 1);
+		maxDepthTopDown(root.right, depth + 1);
+	}
+
+
 }

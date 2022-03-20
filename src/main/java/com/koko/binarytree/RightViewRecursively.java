f1c41be96cb1a40a7b73187f31cd200a99a88095
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-03-20 19:26:35 +0530

    Create RightViewRecursively.java

---
+++ src/main/java/com/koko/binarytree/RightViewRecursively.java
@@ -0,0 +1,28 @@
+package com.koko.binarytree;
+
+import com.koko.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RightViewRecursively {
+
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		rightView(root, 0, result);
+
+		return result;
+	}
+
+
+	public void rightView(TreeNode node, int level, List<Integer> result) {
+		if (node == null) return;
+
+		if (result.size() == level) {
+			result.add(node.val);
+		}
+
+		rightView(node.right, level + 1, result);
+		rightView(node.left, level + 1, result);
+	}
+}

08cd9887ae4a582e58dc822508cf0d68ffc887c7
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2022-03-29 13:52:46 +0530

    Create StrStr.java

---
+++ src/main/java/com/koko/string/StrStr.java
@@ -0,0 +1,36 @@
+package com.koko.string;
+
+public class StrStr {
+	public static int strStr(String haystack, String needle) {
+
+		if (haystack.length() == 0 && needle.length() == 0)
+			return 0;
+
+		if (needle.length() > haystack.length())
+			return -1;
+
+		int n = needle.length();
+
+		for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+			int found = 0;
+			for (int j = 0; j < needle.length(); j++) {
+				if (haystack.charAt(i + j) == needle.charAt(j)) {
+					found++;
+				} else {
+					break;
+				}
+			}
+
+			if (found == needle.length())
+				return i;
+
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		String haystack = "hello", needle = "o";
+		System.out.println(strStr(haystack, needle));
+	}
+}

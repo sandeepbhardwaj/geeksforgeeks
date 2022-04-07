d4b76cc386b7cbe0ba53ecbd073edf8f1f51006c
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2022-04-07 12:07:56 +0530

    Update ValidSudoku.java

--- src/main/java/com/koko/matrix/ValidSudoku.java
+++ src/main/java/com/koko/matrix/ValidSudoku.java
@@ -8,10 +8,10 @@ import java.util.Set;
 public class ValidSudoku {
 	public static boolean isValidSudoku(char[][] board) {
 		Set<String> seen = new HashSet();
-		for (int i = 0; i < 9; ++i) {
-			for (int j = 0; j < 9; ++j) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
 				char number = board[i][j];
-				System.out.print(i / 3 + "-" + j / 3+" | ");
+				System.out.print(i / 3 + "-" + j / 3 + " | ");
 				if (number != '.')
 					if (!seen.add(number + " in row " + i) ||
 							!seen.add(number + " in column " + j) ||

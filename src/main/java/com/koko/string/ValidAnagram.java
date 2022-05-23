851b03a257ab672394e73a641d6774f48fdbad1a
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-05-23 14:08:03 +0530

    Update ValidAnagram.java

--- src/main/java/com/koko/string/ValidAnagram.java
+++ src/main/java/com/koko/string/ValidAnagram.java
@@ -3,28 +3,22 @@ package com.koko.string;
 public class ValidAnagram {
 	public static boolean isAnagram(String s, String t) {
 
-		if (s.length() != t.length())
-			return false;
+		if (s.length() != t.length()) return false;
 
 		int[] map = new int[26];
 
-		for (char ch : s.toCharArray()) {
-			map[ch - 'a']++;
-		}
+		for (char ch : s.toCharArray()) map[ch - 'a']++;
 
-		for (char ch : t.toCharArray()) {
-			map[ch - 'a']--;
-		}
+		for (char ch : t.toCharArray()) map[ch - 'a']--;
 
 		for (int i : map) {
-			if (i != 0)
-				return false;
+			if (i != 0) return false;
 		}
 
 		return true;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("IS abc and bca is anagram :" + isAnagram("abc", "bca"));
+		System.out.println("Is abc and bca is anagram :" + isAnagram("abc", "bca"));
 	}
 }

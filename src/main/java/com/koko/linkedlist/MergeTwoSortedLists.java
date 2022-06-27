d946da4e2c8db0955ecb530a4b26910355395de1
Bhardwaj, Sandeep <sandeep_bhardwaj@optum.com>
2022-06-27 07:36:19 +0530

    Update MergeTwoSortedLists.java

--- src/main/java/com/koko/linkedlist/MergeTwoSortedLists.java
+++ src/main/java/com/koko/linkedlist/MergeTwoSortedLists.java
@@ -39,4 +39,20 @@ public class MergeTwoSortedLists {
 		}
 		return fakeHead.next;
 	}
+
+	public ListNode mergeTwoListsRecursively(ListNode l1, ListNode l2) {
+		if (l1 == null) return l2;
+		if (l2 == null) return l1;
+
+		ListNode handler;
+		if (l1.val < l2.val) {
+			handler = l1;
+			handler.next = mergeTwoLists(l1.next, l2);
+		} else {
+			handler = l2;
+			handler.next = mergeTwoLists(l1, l2.next);
+		}
+
+		return handler;
+	}
 }
